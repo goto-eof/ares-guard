9dd0d8e72dfcd7fe61d985fbd5030b0d8866811c
Andrei Dodu <andrei.dodu@hotmail.it>
2024-07-07 01:55:02 +0200

    feature: path specified as parameter

--- src/main/java/com/andreidodu/aresguard/AresGuardApplication.java
+++ src/main/java/com/andreidodu/aresguard/AresGuardApplication.java
@@ -23,8 +23,8 @@ import java.util.Set;
 public class AresGuardApplication implements CommandLineRunner {
 
 
-    private final String MARKER_START = "# marker-start";
-    private final String MARKER_END = "# marker-end";
+    private final String MARKER_START = "# ares-guard-marker-start";
+    private final String MARKER_END = "# ares-guard-marker-end";
 
 
     private final IpRetrieverService ipRetrieverService;
@@ -38,38 +38,84 @@ public class AresGuardApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        if (args.length != 1) {
+            LOG.error("Please specify the root directory of your website. Example: \"/var/www/mywebsite.com\"|");
+            return;
+        }
+        final String pathString = args[0];
+        LOG.info("selected path: {}", pathString);
         Map<String, List<IpPrefixDTO>> botsAndIPs = this.ipRetrieverService.retrieveAllIPs();
         try {
-            this.writeOnFile(botsAndIPs);
+            this.writeOnFile(pathString, botsAndIPs);
+            this.rewriteRobotsFile(pathString, botsAndIPs);
         } catch (IOException e) {
             LOG.error("Failed to write on .htaccess file: {}", e.toString());
         }
 
     }
 
-    private void writeOnFile(Map<String, List<IpPrefixDTO>> botsAndIPs) throws IOException {
-        Path path = Paths.get("/home/andrei/Desktop/.htaccess");
+    private void rewriteRobotsFile(final String pathString, Map<String, List<IpPrefixDTO>> botsAndIPs) throws IOException {
+        Path path = Paths.get(pathString + "/robots.txt");
 
-        List<String> lst = Files.readAllLines(path);
+        List<String> lst = new ArrayList<>();
+        lst.add("User-agent: *");
+        lst.add("Disallow: /");
+        lst.add("");
 
+        Set<String> keys = botsAndIPs.keySet();
+        keys.forEach(k -> {
+            lst.add("User-agent: " + k);
+            lst.add("Disallow:");
+            lst.add("");
+        });
+
+        Files.write(path, lst);
+
+    }
+
+    private void writeOnFile(final String pathString, Map<String, List<IpPrefixDTO>> botsAndIPs) throws IOException {
+        Path path = Paths.get(pathString + "/.htaccess");
+
+
+        List<String> allExistingLines = new ArrayList<>();
+
+        try {
+            allExistingLines = Files.readAllLines(path);
+        } catch (Exception e) {
+            LOG.warn(".htaccess not found");
+        }
         List<String> generatedRows = generateLines(botsAndIPs);
-        List<String> fullText = insertNewRows(lst, generatedRows);
+        List<String> fullText = insertNewRows(allExistingLines, generatedRows);
 
         Files.write(path, fullText);
+    }
+
+    private void writeIfStart(List<String> allExistingLines) {
+        allExistingLines.add("<IfModule mod_rewrite.c>");
+        allExistingLines.add("RewriteEngine On");
+    }
+
+    private void writeIfEnd(List<String> currentList) {
+        currentList.add("</IfModule>");
     }
 
     private List<String> insertNewRows(List<String> cleanedLst, List<String> generatedRows) {
         List<String> result = new ArrayList<>();
 
         boolean inMarked = false;
+        boolean found = false;
         for (String line : cleanedLst) {
             if (MARKER_START.equalsIgnoreCase(line.trim())) {
                 inMarked = true;
                 result.add(line);
+                writeIfStart(result);
                 result.addAll(generatedRows);
+                found = true;
             } else if (MARKER_END.equalsIgnoreCase(line.trim())) {
+                writeIfEnd(result);
                 result.add(line);
                 inMarked = false;
+
             } else {
                 if (inMarked) {
                     LOG.info("removing: {}", line);
@@ -78,7 +124,13 @@ public class AresGuardApplication implements CommandLineRunner {
                 }
             }
         }
-
+        if (!found) {
+            result.add(MARKER_START);
+            writeIfStart(result);
+            result.addAll(generatedRows);
+            writeIfEnd(result);
+            result.add(MARKER_END);
+        }
         return result;
     }
 
@@ -89,17 +141,19 @@ public class AresGuardApplication implements CommandLineRunner {
         for (String key : keys) {
             String header = "RewriteCond %{HTTP_USER_AGENT} ^(.*)" + key + "(.*)$ [NC]";
             result.add(header);
-            for (IpPrefixDTO ipPrefix : botsAndIPs.get(key)) {
-                String content = "RewriteCond expr \"! %{REMOTE_ADDR} -ipmatch '" + ipPrefix.getPrefix() + "'\"";
-                result.add(content);
-            }
+            writeIpList(botsAndIPs, key, result);
             String footer = "RewriteRule ^ - [R=403,L]";
             result.add(footer);
-
-            LOG.info("{} -> {}", key, botsAndIPs.get(key));
         }
 
         return result;
+    }
+
+    private static void writeIpList(Map<String, List<IpPrefixDTO>> botsAndIPs, String key, List<String> result) {
+        for (IpPrefixDTO ipPrefix : botsAndIPs.get(key)) {
+            String content = "RewriteCond expr \"! %{REMOTE_ADDR} -ipmatch '" + ipPrefix.getPrefix() + "'\"";
+            result.add(content);
+        }
     }
 
 }
